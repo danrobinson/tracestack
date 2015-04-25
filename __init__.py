483fe112bb5115862a924c99ff47311c2bfd5b0f
Daniel Robinson <danrobinson010@gmail.com>
2015-04-25 19:43:42 -0400

    Changed

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import sys, urllib, webbrowser
 
 def tracestack():
     last_error = sys.last_value
-    if not last_error:
+    if last_error is None:
         raise Exception("No error message available.")
     error_query = urllib.encode("[python] " + str(last_error))
     search_url = "http://stackoverflow.com/search?q=" + error_query
