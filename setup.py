33ec5bc6a33777fb095a23bb0bee174b3640a593
Daniel Robinson <danrobinson010@gmail.com>
2015-04-25 19:47:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tracestack',
+      version='0.1',
+      description='Search your last error on Stack Overflow.',
+      author='Dan Robinson',
+      author_email='danrobinson010@gmail.com',
+      url='https://www.github.com/danrobinson/tracestack',
+      packages=['tracestack'],
+     )
