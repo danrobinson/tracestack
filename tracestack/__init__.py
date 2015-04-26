b24a4e8ee32d0bf26b910b5dc308ba27cf5ed4bf
Daniel Robinson <danrobinson010@gmail.com>
2015-04-25 20:03:24 -0400

    Debugged with Aaron's help

--- tracestack/__init__.py
+++ tracestack/__init__.py
@@ -1,10 +1,11 @@
 import sys, urllib, webbrowser
 
 def tracestack():
-    last_error = sys.last_value
-    if last_error is None:
+    try:
+        last_error = sys.last_value
+    except:
         raise Exception("No error message available.")
-    error_query = urllib.encode("[python] " + str(last_error))
+    error_query = urllib.urlencode("[python] " + str(last_error))
     search_url = "http://stackoverflow.com/search?q=" + error_query
     webbrowser.open(search_url)
     
