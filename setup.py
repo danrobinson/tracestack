cfb03d65113a6b2c2f855b3d80550c45178739e3
Dan Robinson <danrobinson010@gmail.com>
2015-08-31 07:20:18 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='tracestack',
-      version='0.1',
+      version='0.1.1',
       description='Search your last error on Stack Overflow.',
       author='Dan Robinson',
       author_email='danrobinson010@gmail.com',
       url='https://www.github.com/danrobinson/tracestack',
-      download_url='https://github.com/danrobinson/tracestack/tarball/0.1',
+      download_url='https://github.com/danrobinson/tracestack/tarball/0.1.1',
       packages=['tracestack'],
       entry_points = {'console_scripts': ['tracestack=tracestack.command_line:run'],}
      )
