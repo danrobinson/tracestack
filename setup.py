6cae9f3b67355af79a82513492c8f9490c704e87
Dan Robinson <danrobinson010@gmail.com>
2015-09-19 09:13:02 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ except(IOError, ImportError):
 from setuptools import setup
 
 setup(name='tracestack',
-      version='0.1.8',
+      version='0.1.9',
       description='Instantly search your Python error messages on the web.',
       author='Dan Robinson',
       author_email='danrobinson010@gmail.com',
       url='https://www.github.com/danrobinson/tracestack',
-      download_url='https://github.com/danrobinson/tracestack/tarball/0.1.8',
+      download_url='https://github.com/danrobinson/tracestack/tarball/0.1.9',
       long_description=long_description,
       packages=['tracestack'],
       test_suite="tests",
